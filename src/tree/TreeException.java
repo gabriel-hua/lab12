99fd962c486120e307cabbe13c4885712d2364ce
gabriel-hua <gmasterhua@gmail.com>
2026-04-24 08:35:43 -0700

    Initial Commit

---
+++ src/tree/TreeException.java
@@ -0,0 +1,9 @@
+package tree;
+
+public class TreeException extends Exception {
+    public TreeException(String s) {
+        // Pass s into a superclass ctor.
+        super("Tree error.");
+
+    }
+}
